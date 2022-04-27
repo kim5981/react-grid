ea8472aa464e015a7f846f175d97a6da8b5e8796
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-04-26 21:20:19 -0700

    updated functional component to match class component

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,37 +1,187 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
+
+const URL = `http://localhost:9000/api/result`
 
 export default function AppFunctional(props) {
-  return (
-    <div id="wrapper" className={props.className}>
-      <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+  const [ state, setState ] =  useState({
+    steps: 0,
+    x: 2,
+    y: 2,
+    B: "",
+    email: "",
+    message: "" ,
+  })
+
+  const clickLeft = () => {
+    if ( state.x === 1) {
+      setState({
+        ...state,
+        message: "You can't go left"
+      })
+    } else if ( state.x > 1 && state.x <= 3 ) {
+      setState({
+        ...state,
+        steps: state.steps + 1,
+        x: state.x - 1
+      })
+    }
+  }
+
+  const clickRight = () => {
+    if ( state.x === 3 ) {
+      setState({
+        ...state,
+        message: "You can't go right",
+      })
+    } else if ( state.x >= 1 || state.x <= 3 ) {
+      setState({
+        ...state,
+        steps: state.steps + 1,
+        x: state.x + 1,
+        message: ""
+      })
+    }
+  }
+
+  const clickUp = () => {
+    if ( state.y === 1 ) {
+      setState({
+        ...state,
+        message: "You can't go up",
+      })
+    } else if ( state.y <= 3 ) {
+      setState({
+        ...state,
+        steps: state.steps + 1,
+        y: state.y - 1,
+        message: ""
+      })
+    }
+  }
+
+  const clickDown = () => {
+    if( state.y === 3) {
+      setState({
+        ...state,
+        message: "You can't go down"
+      })
+    } else if ( state.y >= 1 || state.y <= 3 ) {
+      setState({
+        ...state,
+        steps: state.steps + 1,
+        y: state.y + 1 
+      })
+    }
+    } 
+
+
+  const reset = () => {
+    setState({
+      steps: 0,
+      message: "",
+      x: 2,
+      y: 2,
+      email: ""
+    })
+  }
+
+  const onChange = evt => {
+    const { value } = evt.target
+    console.log(value)
+    setState({
+      ...state, 
+      email: value
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    axios.post( URL, { ...state, email: state.email })
+    .then( res => {
+      setState({ ...state, message: res.data.message })
+      setState({ ...state, email: "" })
+    })
+    .catch( err => {
+      setState({ ...state, message: err.response.data.message })
+    })
+  
+  }
+
+    return (
+      <div id="wrapper" className={props.className}>
+
+        <div className="info">
+          <h3 id="coordinates">{`Coordinates (${ state.x }, ${ state.y })`}</h3>
+          <h3 id="steps">{`You moved ${state.steps} times`}</h3>
+        </div>
+
+        <div id="grid">
+
+        <div className={ `${ state.x === 1 && state.y === 1 ? "square active" : "square" } ` }>
+           { state.x === 1 && state.y === 1 ? "B" : "" }
+        </div>
+
+        <div className={ `${ state.x === 2 && state.y === 1 ? "square active" : "square" } ` }>
+           { state.x === 2 && state.y === 1 ? "B" : "" }
+        </div>
+
+        <div className={ `${ state.x === 3 && state.y === 1 ? "square active" : "square" } ` }>
+           { state.x === 3 && state.y === 1 ? "B" : "" } 
+        </div>
+
+        <div className={ `${ state.x === 1 && state.y === 2 ? "square active" : "square" } ` }>
+           { state.x === 1 && state.y === 2 ? "B" : "" } 
+        </div>
+
+        <div className={ `${ state.x === 2 && state.y === 2 ? "square active" : "square" } ` }> 
+          {  state.x === 2 && state.y === 2 ? "B" : "" } 
+        </div>
+
+        <div className={ `${ state.x === 3 && state.y === 2 ? "square active" : "square" } ` }> 
+          { state.x === 3 && state.y === 2 ? "B" : "" } 
+        </div>
+
+        <div className={ `${ state.x === 1 && state.y === 3 ? "square active" : "square" } ` }>
+          { state.x === 1 && state.y === 3 ? "B" : "" }
+        </div>
+
+        <div className={ `${ state.x === 2 && state.y === 3 ? "square active" : "square" } ` }>
+          { state.x === 2 && state.y === 3 ? "B" : "" }
+        </div>
+
+        <div className={ `${ state.x === 3 && state.y === 3 ? "square active" : "square" } ` }> 
+          { state.x === 3 && state.y === 3 ? "B" : "" } 
+        </div>
+         
+        </div>
+
+        <div className="info">
+          <h3 id="message">{ state.message }</h3>
+        </div>
+
+        <div id="keypad">
+          <button onClick={ () => clickLeft() } id="left">LEFT</button>
+          <button onClick={ () => clickUp() } id="up">UP</button>
+          <button onClick={ () => clickRight() } id="right">RIGHT</button>
+          <button onClick={ () => clickDown() } id="down">DOWN</button>
+          <button onClick={ reset } id="reset">reset</button>
+        </div>
+
+        <form onSubmit={ onSubmit }>
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="type email"
+            value={ state.email }
+            onChange={ onChange }
+          />
+          <input
+            id="submit" 
+            type="submit"
+          />
+        </form>
       </div>
-      <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-      </div>
-      <div className="info">
-        <h3 id="message"></h3>
-      </div>
-      <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
-      </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
-      </form>
-    </div>
-  )
+    )
+  
 }
